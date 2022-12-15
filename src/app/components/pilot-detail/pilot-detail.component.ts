9576aa9be3a51b3b85516e2594930c498c050b51
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-12-15 23:02:50 +0100

    pilot details gets info pilot

--- src/app/components/pilot-detail/pilot-detail.component.ts
+++ src/app/components/pilot-detail/pilot-detail.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pilot } from 'src/app/model/pilot';
+import { StarshipsService } from 'src/app/services/starships.service';
 
 @Component({
   selector: 'app-pilot-detail',
   templateUrl: './pilot-detail.component.html',
   styleUrls: ['./pilot-detail.component.css']
 })
-export class PilotDetailComponent {
+export class PilotDetailComponent implements OnInit {
 
+  pilotDetails!: Pilot;
+  urlRoute!: string;
+  constructor(private serviceStarships: StarshipsService,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.urlRoute = params['url'];
+    });
+    console.log(this.urlRoute);
+    this.getPilotSelected();
+  }
+
+  getPilotSelected() {
+    this.serviceStarships.getResource(this.urlRoute).subscribe(data => {
+      this.pilotDetails = data;
+     
+      console.log(this.pilotDetails);
+    });
+  }
 }
--- src/app/components/pilots/pilots.component.ts
+++ src/app/components/pilots/pilots.component.ts
@@ -3,7 +3,8 @@ import { Pilot } from 'src/app/model/pilot';
 import { Starship } from 'src/app/model/starship';
 import { StarshipsService } from 'src/app/services/starships.service';
 import { Router } from '@angular/router';
-
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-pilots',
   templateUrl: './pilots.component.html',
@@ -15,11 +16,10 @@ export class PilotsComponent implements OnInit {
 
   pilots: Pilot[] = [];
   characterId!: string;
-  // pilotsIds: string[]=[];
+  pilot!: Pilot;
 
-  constructor(private serviceStarships: StarshipsService, private router: Router) {
 
-  }
+  constructor(private serviceStarships: StarshipsService, private router: Router) {}
 
   public handleMissingImage(event: Event) {
     (event.target as HTMLImageElement).src = "../../../../../assets/img/starships/big-placeholder.jpg";
@@ -33,23 +33,26 @@ export class PilotsComponent implements OnInit {
   getPilots(): void {
     this.starshipDetails.pilots!.forEach((pilot: string) => {
 
-     
+
       //this.pilotsIds.push(this.characterId);
       // console.log(this.pilotsIds);
 
       this.serviceStarships.getResource(pilot).subscribe((data: any) => {
-    
-       data.id = data.url.split('/')[5];
-      console.log(data.id);
-        console.log(data.id);
+
+        data.id = data.url.split('/')[5];
         this.pilots.push(data);
+
 
       });
     });
   }
 
-  navigate() {
-    this.router.navigate(['/pilotDetail']);
+
+
+
+  getDetail(e:any) {
+    console.log(e);
+    this.router.navigate(['/pilotDetail'], {queryParams: {url: e.url}});
   }
 
 
