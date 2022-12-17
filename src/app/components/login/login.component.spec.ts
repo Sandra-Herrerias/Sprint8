84cb8fdd2dd75abcdf2cd715e0306ecf345ee7e8
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-12-17 11:09:45 +0100

    login unit tests

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,12 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
+import { DebugElement } from '@angular/core';
+import { BrowserModule, By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +19,18 @@ describe('LoginComponent', () => {
         RouterTestingModule,
         ReactiveFormsModule,
         FormsModule,
-    ],
-      declarations: [ LoginComponent ]
+        BrowserModule
+      ],
+      declarations: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents().then(() => {
+
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+
+        de = fixture.debugElement.query(By.css('form'));
+        el = de.nativeElement;
+      });
   });
 
   beforeEach(() => {
@@ -29,4 +42,16 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form should be invalid', async(() => {
+    component.loginForm.controls['username'].setValue('');
+    component.loginForm.controls['password'].setValue('');
+    expect(component.loginForm.valid).toBeFalsy();
+  }));
+
+  it('form should be valid', async(() => {
+    component.loginForm.controls['username'].setValue('sandra@gmail.com');
+    component.loginForm.controls['password'].setValue('123456');
+    expect(component.loginForm.valid).toBeTruthy();
+  }));
 });
